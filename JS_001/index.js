e88baa6aff65b386a119ce5a63c4de861d3d2fa4
NdBR <84131697+NdBR@users.noreply.github.com>
2021-06-23 14:11:21 +0200

    Add files via upload

---
+++ JS_001/index.js
@@ -0,0 +1,9 @@
+console.log("Hello Winc Academy");
+let name = "niels";
+console.log(name)
+let text1 = "Good";
+let text2 = " Morning";
+let text3 = text1 + text2;
+console.log(text3);
+let age = "zesendertig";
+console.log(age);
